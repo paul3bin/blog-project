9c34f0f4959a9a30d5a806166e8e25f2a44b6a8f
Ebin Paul <ebinpaul@outlook.com>
2020-09-09 15:01:56 +0530

    Update on html files

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -64,7 +64,7 @@ class DraftListView(LoginRequiredMixin, ListView):
 @login_required
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    post.publish
+    post.publish()
     return redirect('post_detail', pk=pk)
 
 # FBV for adding a comment to the blogpost
