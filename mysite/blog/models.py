ff86478eeb6d4a65f70e6e77262cbd39b7eeee22
Ebin Paul <ebinpaul@outlook.com>
2020-09-09 15:38:35 +0530

    update

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -32,7 +32,7 @@ class Comments(models.Model):
     approved_comment = models.BooleanField(default=False)
 
     def approve(self):
-        self.approve_comment = True
+        self.approved_comment = True
         self.save()
 
     def get_absolute_url(self):
